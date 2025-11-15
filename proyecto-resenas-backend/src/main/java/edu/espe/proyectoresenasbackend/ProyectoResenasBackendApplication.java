18a9ba29a7b6d0e7bb15f73e83e4ac6de8d0e704
Oscarf05 <ofchanataxi@espe.edu.ec>
2025-11-15 13:19:33 -0500

    Estructura inicial

---
+++ proyecto-resenas-backend/src/main/java/edu/espe/proyectoresenasbackend/ProyectoResenasBackendApplication.java
@@ -0,0 +1,13 @@
+package edu.espe.proyectoresenasbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoResenasBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProyectoResenasBackendApplication.class, args);
+    }
+
+}
---
+++ proyecto-resenas-backend/src/test/java/edu/espe/proyectoresenasbackend/ProyectoResenasBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package edu.espe.proyectoresenasbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoResenasBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
