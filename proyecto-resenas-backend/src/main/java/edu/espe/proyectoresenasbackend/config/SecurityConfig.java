ffd627d8e54773f90cfcd3bad5748b6884d120b1
Oscarf05 <ofchanataxi@espe.edu.ec>
2025-11-16 13:06:03 -0500

    Configurar despliegue frontend

--- proyecto-resenas-backend/src/main/java/edu/espe/proyectoresenasbackend/config/SecurityConfig.java
+++ proyecto-resenas-backend/src/main/java/edu/espe/proyectoresenasbackend/config/SecurityConfig.java
@@ -55,7 +55,7 @@ public class SecurityConfig {
     @Bean
     public CorsConfigurationSource corsConfigurationSource() {
         CorsConfiguration configuration = new CorsConfiguration();
-        configuration.setAllowedOrigins(List.of("*")); // Tu front local y el de prod
+        configuration.setAllowedOrigins(List.of("http://localhost:5173", "https://proyecto-resenas-frontend.onrender.com/*")); // Tu front local y el de prod
         configuration.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         configuration.setAllowedHeaders(List.of("Authorization", "Content-Type"));
         configuration.setAllowCredentials(true);
