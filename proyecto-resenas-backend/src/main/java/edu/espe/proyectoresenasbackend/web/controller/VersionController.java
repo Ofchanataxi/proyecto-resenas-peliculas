3a8bb999496375b164b2fb8c3d45479c749dd1d8
Oscarf05 <ofchanataxi@espe.edu.ec>
2025-11-16 13:23:19 -0500

    Agregar configuracion para conexion desde front

--- proyecto-resenas-backend/src/main/java/edu/espe/proyectoresenasbackend/web/controller/VersionController.java
+++ proyecto-resenas-backend/src/main/java/edu/espe/proyectoresenasbackend/web/controller/VersionController.java
@@ -1,6 +1,7 @@
 package edu.espe.proyectoresenasbackend.web.controller;
 
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,7 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/resenas")
-@CrossOrigin(origins = "*")
+@CrossOrigin("*")
 public class VersionController {
     // Estos valores se inyectan desde application.yml
     @Value("${app.name}")
