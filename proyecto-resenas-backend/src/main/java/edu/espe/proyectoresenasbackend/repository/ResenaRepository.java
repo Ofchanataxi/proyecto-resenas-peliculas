3ea63a87a95670c35986533311fa93dc8279cb69
Johan <johan@gmail.com>
2025-11-17 15:52:20 -0500

    domain,servicios,controladores v3

--- proyecto-resenas-backend/src/main/java/edu/espe/proyectoresenasbackend/repository/ResenaRepository.java
+++ proyecto-resenas-backend/src/main/java/edu/espe/proyectoresenasbackend/repository/ResenaRepository.java
@@ -5,6 +5,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
 
+//Repositorio para la entidad Resena
 public interface ResenaRepository extends JpaRepository<Resena, Long> {
     List<Resena> findByPeliculaId(Long peliculaId);
 }
--- proyecto-resenas-backend/src/main/java/edu/espe/proyectoresenasbackend/web/controller/ResenaController.java
+++ proyecto-resenas-backend/src/main/java/edu/espe/proyectoresenasbackend/web/controller/ResenaController.java
@@ -7,6 +7,7 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+//Controlador REST para la entidad Resena
 @RestController
 @RequestMapping("/api/resenas/resena")
 public class ResenaController {
