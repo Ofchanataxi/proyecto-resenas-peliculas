4aff294bc2941fe0372d577ada88275b65c02f18
ofchanataxi <ofchanataxi@espe.edu.ec>
2025-11-17 09:07:06 -0500

    Change API endpoint from '/api/resenas' to '/api/resena/resenas'

--- proyecto-resenas-backend/src/main/java/edu/espe/proyectoresenasbackend/web/controller/ResenaController.java
+++ proyecto-resenas-backend/src/main/java/edu/espe/proyectoresenasbackend/web/controller/ResenaController.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/resenas")
+@RequestMapping("/api/resena/resenas")
 public class ResenaController {
     private final ResenaService service;
 
