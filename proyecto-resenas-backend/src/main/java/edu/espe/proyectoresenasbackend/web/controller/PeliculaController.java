9fd7298d6eaf2e57e3da685754bcd5a08aed18ea
ofchanataxi <ofchanataxi@espe.edu.ec>
2025-11-17 13:33:57 -0500

    Update PeliculaController.java

--- proyecto-resenas-backend/src/main/java/edu/espe/proyectoresenasbackend/web/controller/PeliculaController.java
+++ proyecto-resenas-backend/src/main/java/edu/espe/proyectoresenasbackend/web/controller/PeliculaController.java
@@ -11,7 +11,7 @@ import org.springframework.web.bind.annotation.*;
 import java.util.List;
 
 @RestController
-@RequestMapping("/api/resenas/peliculas")
+@RequestMapping("/api/resenas/pelicula")
 public class PeliculaController {
     private final PeliculaService service;
 
